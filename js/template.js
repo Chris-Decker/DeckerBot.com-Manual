a38d51bbcdc69c07628c12e4890be1b952b8e3c5
chris-decker-volusion <chris_decker@volusion.com>
2017-11-03 11:26:54 -0500

    Backup of site to GitHub

---
+++ js/template.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+	/* Loads header/footer files */
+	$("header").load("/pages/header.html");
+	$("footer").load("/pages/footer.html");
+
+	/* Mobile menu toggle */
+	setTimeout(function () {
+		$('.menu-icon').click(function(){
+			$('.subnav').toggleClass('expand');
+			$('.menu-mask').toggleClass('visible');
+		});
+		$('.menu-mask').click(function(){
+			$('.subnav').toggleClass('expand');
+			$('.menu-mask').toggleClass('visible');
+		});
+	}, 100);
+
+	/* Domain toggle for proofs */
+	$('.link').click(function(){
+		$('.link').removeClass('selected');
+		$(this).addClass('selected');
+		domain = $(this).text();
+		siteDesktop = '<img class="proof" src="/images/proofs/' + domain + '/desktop.jpg">';
+		siteTablet = '<img class="proof" src="/images/proofs/' + domain + '/tablet.jpg">';
+		siteMobile = '<img class="proof" src="/images/proofs/' + domain + '/mobile.jpg">';
+		if(domain[0]) {
+			$(".desktop-screen").html(siteDesktop);
+			$('.tablet-screen').html(siteTablet);
+			$('.mobile-screen').html(siteMobile);
+		}
+	});
+
+	/* See all sites */
+	$('.see-all').click(function(){
+		$('.link').removeClass('hide');
+	});
+
+	/* Device display toggle */
+	$('.desktop-icon').click(function(){
+		$('.device').removeClass('visible');
+		$('.desktop').addClass('visible');
+		$('.device-icons').removeClass('active');
+		$('.desktop-icon').addClass('active');
+		$('.device-icons-btn').removeClass('active');
+		$('.scroll-notice').removeClass('visible');
+	});
+	$('.tablet-icon, .mobile-icon').click(function(){
+		$('.device').addClass('visible');
+		$('.desktop').removeClass('visible');
+		$('.device-icons').addClass('active');
+		$('.desktop-icon').removeClass('active');
+		$('.device-icons-btn').removeClass('active');
+		$('.scroll-notice').removeClass('visible');
+	});
+	$('.device-icons-btn').click(function(){
+		if($('.desktop.visible').length) {
+			$('.scroll-notice').addClass('visible');
+		}
+		$('.device-icons-btn').addClass('active');
+		$('.device').addClass('visible');
+		$('.device-icons').addClass('active');
+	});
+});
